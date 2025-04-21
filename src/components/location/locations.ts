d2baa1b687dc7b7d7eabeebffee5aa8d6ef1c143
Luis U. <luis.urruela@neweratech.com>
2025-04-21 11:18:15 -0600

    Updated location page. Added responsive styles and separated page sections in single components

---
+++ src/components/location/locations.ts
@@ -0,0 +1,229 @@
+export const locations = [
+  {
+    zone: "America",
+    countries: [
+      {
+        country: "Estados Unidos",
+        offices: [
+          {
+            addressLines: ["4695 Chabot Dr.", "Suite 102 Pleasanton", "CA 94588", "United States"],
+          },
+          {
+            addressLines: ["11450 Knott Street", "Garden Grove", "CA 92841", "United States"],
+          },
+          {
+            addressLines: ["7 Progress Drive", "Cromwell", "CT 06416", "United States"],
+          },
+          {
+            addressLines: ["4631 Woodland Corporate Blvd.", "Suite 101 Tampa", "FL 33614", "United States"],
+          },
+          {
+            addressLines: ["901 Warrenville Road", "UNIT 210, Lisle", "IL 60532", "United States"],
+          },
+          {
+            addressLines: ["7506 Honeywell Drive", "Fort Wayne", "IN 46825", "United States"],
+          },
+          {
+            addressLines: ["8940 Vincennes Circle", "Indianapolis", "IN 46268", "United States"],
+          },
+          {
+            addressLines: ["13565 McKinley Highway", "Suite 4 Mishawaka", "IN 46545", "United States"],
+          },
+          {
+            addressLines: ["301 Pennsylvania Parkway", "Carmel", "IN 46280", "United States"],
+          },
+          {
+            addressLines: ["3209 Union Pike", "Richmond", "IN 47374", "United States"],
+          },
+          {
+            addressLines: ["2 Batterymarch Park", "Suite 401 Quincy", "MA 02169", "United States"],
+          },
+          {
+            addressLines: ["70 Shawmut Rd", "Canton", "MA 02021", "United States"],
+          },
+          {
+            addressLines: ["1405 Xenium Lane N.", "Suite 120, Plymouth", "MN 55441", "United States"],
+          },
+          {
+            addressLines: ["5929 Baker Road", "Suite 475 Minnetonka", "MN 55345", "United States"],
+          },
+          {
+            addressLines: ["3530 Toringdon Way", "404, Charlotte", "NC 28277", "United States"],
+          },
+          {
+            addressLines: ["111 Maplewood Avenue", "Portsmouth", "NH 03801", "United States"],
+          },
+          {
+            addressLines: ["111 Moonachie Avenue", "Unit 6 Caesar Place Moonachie", "NJ 07074", "United States"],
+          },
+          {
+            addressLines: ["80 Route 4", "Suite 400, Paramus", "NJ 07652", "United States"],
+          },
+          {
+            addressLines: ["7255 Bermuda Road", "Suite D, Las Vegas", "NV 89119", "United States"],
+          },
+          {
+            addressLines: ["2950 Express Drive South", "Suite 112, Islandia", "NY 11749", "United States"],
+          },
+          {
+            addressLines: ["1370 Avenue of the Americas", "10th Floor New York", "NY 10019", "United States"],
+          },
+          {
+            addressLines: ["4445 Lake Forest Drive", "440, Cincinnati", "OH 45242", "United States"],
+          },
+          {
+            addressLines: ["3445 Millennium Court", "Columbus", "OH 43219", "United States"],
+          },
+          {
+            addressLines: ["8940 Lyra Drive 220", "Columbus", "OH 43240", "United States"],
+          },
+          {
+            addressLines: ["1715 Indian Wood Circle", "Suite 260 Maumee", "OH 43537", "United States"],
+          },
+          {
+            addressLines: ["9078 Union Centre Blvd", "Suite 200 West Chester Township", "OH 45069", "United States"],
+          },
+          {
+            addressLines: ["901 S. Bolmar St.", "Bldg. 1, Suite G", "West Chester, PA 19382", "United States"],
+          },
+          {
+            addressLines: ["2201 E Enterprise Avenue", "Suite 201 Appleton", "WI 54913", "United States"],
+          },
+          {
+            addressLines: ["210 McClellan St", "Suite 100, 200, 201, 207, 307, 310", "Wausau, WI 54403", "United States"],
+          },
+          {
+            addressLines: ["10400 West Innovation Drive", "Suite 100 Wauwatosa", "WI 53226", "United States"],
+          },
+        ],
+      },
+      {
+        country: "Brasil",
+        offices: [
+          {
+            addressLines: ["Rua Arquiteto Olavo Redig de Campos, 105", "Torre B – 24º Andar", "Chácara Santo Antônio", "São Paulo – SP, 04711-905", "Brazil"],
+          },
+        ],
+      },
+      {
+        country: "Canada",
+        offices: [
+          {
+            addressLines: ["1430 Boulevard Saint-Martin Ouest", "Laval, QC H7S 1M9", "Canada"],
+          },
+          {
+            addressLines: ["95 Mural St. Suite 600", "Richmond Hill, ON L4B 3G2", "Canada"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    zone: "Reino Unido",
+    offices: [
+      {
+        addressLines: ["17-23 High Street", "Slough", "Berkshire SL11DY", "UK"],
+      },
+    ],
+  },
+  {
+    zone: "Europa",
+    countries: [
+      {
+        country: "Bélgica",
+        offices: [
+          {
+            addressLines: ["New Era Technology BE NV/SA", "Leuvensesteenweg 325", "1932 Zaventem", "Belgium"],
+          },
+        ],
+      },
+      {
+        country: "Rumania",
+        offices: [
+          {
+            addressLines: ["78 Constantin Brancusi st", "Cluj-Napoca", "Romania"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    zone: "Asia - Pacífico",
+    countries: [
+      {
+        country: "Australia",
+        offices: [
+          {
+            addressLines: ["Suite 9.04, 30 Currie Street", "Adelaide, SA 5000", "Australia"],
+          },
+          {
+            addressLines: ["Unit 6, 43-49 Sandgate Road", "Albion, Brisbane QLD 4010", "Australia"],
+          },
+          {
+            addressLines: ["Unit 1 & 2, 140 Abernethy Road", "Belmont, Perth WA 6105", "Australia"],
+          },
+          {
+            addressLines: ["Suite 4/321, Camberwell Road", "Camberwell, Melbourne VIC 3124", "Australia"],
+          },
+          {
+            addressLines: ["Highpoint Building", "257/259 Central Coast Hwy,", "Erina, NSW 2250", "Australia"],
+          },
+          {
+            addressLines: ["Suite 7.02,", "1 Chalmers Crescent", "Mascot, Sydney NSW 2020", "Australia"],
+          },
+          {
+            addressLines: ["First Floor Suite 3, 160 Bridge Street", "Muswellbrook, NSW 2333", "Australia"],
+          },
+          {
+            addressLines: ["75 Shortland Esplanade", "Newcastle, NSW 2300", "Australia"],
+          },
+          {
+            addressLines: ["8 Manning Street", "Taree, NSW 2430", "Australia"],
+          },
+        ],
+      },
+      {
+        country: "Filipinas",
+        offices: [
+          {
+            addressLines: ["18/F Multinational Bancorporation Centre", "6805 Ayala Avenue", "Makati City, 1226", "Philippines"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    zone: "Centros de Operaciones",
+    countries: [
+      {
+        country: "Centro de Operaciones",
+        offices: [
+          {
+            addressLines: ["Boston, MA, United States", "Melbourne, Australia", "Auckland, New Zealand", "Brighton, United Kingdom"],
+          },
+        ],
+      },
+      {
+        country: "Centro de Operaciones AV",
+        offices: [
+          {
+            addressLines: ["West Chester, PA", "United States"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    zone: "Mena",
+    countries: [
+      {
+        country: "Emiratos Árabes",
+        offices: [
+          {
+            addressLines: ["#1703, Westburry Office Tower,", "Burj Khalifa District, Business Bay,", "PO Box : 32705,", "Dubai, UAE"],
+          },
+        ],
+      },
+    ],
+  },
+];
