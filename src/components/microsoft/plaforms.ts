9fd892d6c39a5e4ea7f2992c9d0ade56390d2413
Luis U. <luis.urruela@neweratech.com>
2025-04-21 13:05:34 -0600

    Added microsoft page

---
+++ src/components/microsoft/plaforms.ts
@@ -0,0 +1,23 @@
+export const cardConfig = [
+  {
+    icon: "sharepoint-1.png",
+    title: "SharePoint",
+    link: "/soluciones/microsoft/sharepoint",
+    body: "Solución unificada para la creación de sistemas colaborativos de gestión documental, intranets y extranets, y flujos de trabajo automatizados.",
+    classes: "border-0 hover-light",
+  },
+  {
+    icon: "power-bi.png",
+    title: "Power BI",
+    link: "/soluciones/microsoft/power-bi",
+    body: "Aprovecha el poder del Business Intelligence para obtener perspectivas enriquecedoras de tus datos operativos y tomar mejores decisiones basadas en datos.",
+    classes: "border-0 hover-light",
+  },
+  {
+    icon: "power-automate-1.png",
+    title: "Power Automate",
+    link: "/soluciones/microsoft/power-automate",
+    body: "Optimiza y mejora tus operaciones con la automatización para reducir los procesos manuales y repetitivos.",
+    classes: "border-0 hover-light",
+  }
+];
