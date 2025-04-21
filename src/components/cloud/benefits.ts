d7e5e55cd5df9460a82e65291c5c4bc5165ec81f
Luis U. <luis.urruela@neweratech.com>
2025-04-21 14:39:42 -0600

    Added Cloud page

---
+++ src/components/cloud/benefits.ts
@@ -0,0 +1,44 @@
+export const cardConfig = [
+  {
+    icon: "mobility.png",
+    title: "Movilidad",
+    body: "Acceder a datos y aplicaciones ya no es una limitación al usar la Nube. Las aplicaciones y los datos alojados en la nube se pueden acceder en cualquier momento y en cualquier lugar a través de todos los dispositivos, brindando a tus usuarios la movilidad que tanto necesitan.",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "security.png",
+    title: "Securidad",
+    body: "Las plataformas en la nube como Azure ofrecen seguridad integral para sus datos y aplicaciones, con capacidades integradas que le brindan una postura de seguridad sólida. Usted se beneficiará de una mayor seguridad en comparación con las implementaciones locales heredadas, vulnerables a incidentes de seguridad que pueden comprometer sus operaciones comerciales.",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "scalability.png",
+    title: "Escalabilidad",
+    body: "Con la implementación basada en la nube, puede escalar su consumo a medida que crece su negocio. Las limitaciones de agregar más infraestructura física asociadas con altos costos iniciales ya no son un factor que afecta sus operaciones comerciales.",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "flexibility.png",
+    title: "Felxibilidad",
+    body: "En comparación con un mayor TCO en el uso de infraestructura local, la computación en la nube funciona con suscripciones escalables de pago por uso. Esto elimina múltiples costos de licencia con una única suscripción para todas sus necesidades comerciales.",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "choice-of-platforms.png",
+    title: "Elección de Plataforma",
+    body: "Tiene la ventaja de utilizar una variedad de entornos de nube como Azure, AWS y Google Cloud, en los modelos de suscripción que desee, o elegir un modelo de implementación híbrido, que es una combinación de modelos de implementación local y en la nube.",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "integration-and-extensibility.png",
+    title: "Integración y Extensibilidad",
+    body: "La computación en la nube le brinda la ventaja de integrarse con muchas aplicaciones de terceros y fuentes de datos que su empresa utiliza actualmente. Esto le brinda la ventaja de utilizar su pila de tecnología existente para conectarse con sus aplicaciones implementadas en la nube, brindando una continuidad comercial perfecta.",
+    color: "bg-light",
+    classes: "border-0",
+  }
+];
---
+++ src/components/cloud/capabilities.ts
@@ -0,0 +1,56 @@
+export const cardConfig = [
+  {
+    icon: "cloud-enablement.png",
+    title: "Habilitación de la Nube",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "kubernetes-services.png",
+    title: "Servicios de Kubernetes",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "application-modernization.png",
+    title: "Modernización de Aplicaciones",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "identity-and-access-management.png",
+    title: "Gestión de Identidad y Acceso",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "cloud-infrastructure.png",
+    title: "Servicios de Infraestructura en la Nube",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "cloud-finops-cost-optimization.png",
+    title: "Cloud FinOps (Optimización de Costos en la Nube)",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "cloud-migration-infrastructure-applications.png",
+    title: "Migración a la Nube – Infraestructura y Aplicaciones",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "cloud-strategy-consulting-services.png",
+    title: "Consultoría y Servicios de Estrategia en la Nube",
+    color: "bg-light",
+    classes: "border-0",
+  },
+  {
+    icon: "cloud-infrastructure-managed-services.png",
+    title: "Servicios Gestionados de Infraestructura en la Nube",
+    color: "bg-light",
+    classes: "border-0",
+  },
+];
