fec20c4e6fce994e2f5a0c76850d53d14c1d2ba4
Luis U. <luis.urruela@neweratech.com>
2025-04-21 13:05:34 -0600

    Added microsoft page

---
+++ src/components/microsoft/services.ts
@@ -0,0 +1,62 @@
+export const cardConfig = [
+  {
+    icon: "sharepoint-service-offerings.png",
+    title: "Ofertas de Servicios de SharePoint",
+    body: "New Era ofrece una amplia gama de soluciones de SharePoint para tus necesidades personalizadas. Ya sea un flujo de trabajo automatizado, un sistema de gestión documental, una plataforma de colaboración, sistemas de gestión de contenido o paneles de BI integrados, diseñamos nuestras soluciones para que se alineen con tus diversas necesidades empresariales. Nuestra experiencia incluye servidores on-premise y de SharePoint en las versiones 2013, 2019 y Microsoft 365.",
+    classes: "border-0",
+  },
+  {
+    icon: "microsoft-business-solutions.png",
+    title: "Soluciones Empresariales de Microsoft",
+    body: "Ofrecemos servicios de implementación personalizados para Microsoft Dynamics y CRM para brindarte la ventaja completa de tener una solución unificada, reemplazando tu actual conjunto de tecnologías de plataformas dispares y heredadas. Con el beneficio comprobado de reducir los gastos generales de administración y mantenimiento, nuestras implementaciones personalizadas aportan agilidad, mayor productividad y una fácil obtención de beneficios.",
+    classes: "border-0",
+  },
+  {
+    icon: "custom-business-solutions.png",
+    title: "Soluciones Empresariales Personalizadas",
+    body: "Existen casos en los que una aplicación preconstruida no puede abordar eficazmente tus requerimientos empresariales. Tales casos necesitan una aplicación construida a medida que se alinee con tus necesidades operacionales. Ofrecemos desarrollo de aplicaciones personalizadas en ASP.NET, MVC, servicios de Azure, WPF y Silverlight.",
+    classes: "border-0",
+  },
+  {
+    icon: "business-intelligence-dashboards-and-reporting.png",
+    title: "Paneles de Business Intelligence e Informes",
+    body: "New Era comprende la importancia de analizar datos y utilizar los conocimientos derivados para mejorar los procesos empresariales actuales y prepararte para el crecimiento futuro. Con nuestras soluciones de Business Intelligence e Informes, te permitimos profundizar en tus datos con tecnologías modernas como Inteligencia Artificial (IA), Aprendizaje Automático (ML), Procesamiento del Lenguaje Natural (PNL) y Análisis Predictivo.",
+    classes: "border-0",
+  },
+  {
+    icon: "desktop-os-and-application-support-services.png",
+    title: "Servicios de Soporte de Sistemas Operativos de Escritorio y Aplicaciones",
+    body: "Nuestro equipo de profesionales certificados por Microsoft puede encargarse de las implementaciones de tu Sistema Operativo (SO), las actualizaciones de aplicaciones, el mantenimiento y la resolución de problemas de tus equipos de escritorio y portátiles, además de proporcionar soporte a tiempo y bajo demanda.",
+    classes: "border-0",
+  },
+  {
+    icon: "core-infrastructure-services.png",
+    title: "Servicios de Infraestructura Central",
+    body: "Proporcionamos servicios de infraestructura central, como para tus servidores Windows, centrándonos en la accesibilidad y seguridad de tu red y datos. Nuestra probada experiencia en el diseño, implementación y mantenimiento de infraestructura tecnológica garantiza un soporte y una seguridad sin problemas mediante el uso de las últimas mejores prácticas de la industria.",
+    classes: "border-0",
+  },
+  {
+    icon: "mobility-services.png",
+    title: "Servicios de Movilidad",
+    body: "Nuestros servicios de Movilidad hacen que tus aplicaciones sean accesibles desde cualquier dispositivo, como equipos de escritorio, tabletas y teléfonos móviles, utilizando HTML5 e innovaciones táctiles. La movilidad brinda a tus empleados el acceso tan necesario en cualquier momento y lugar, y crea una buena experiencia de usuario y cliente.",
+    classes: "border-0",
+  },
+  {
+    icon: "project-and-portfolio-management.png",
+    title: "Gestión de Proyectos y Portafolio",
+    body: "Los servicios de Gestión de Proyectos y Portafolio de New Era aprovechan la suite de Microsoft Project y Project Online para optimizar la gestión de tus proyectos y portafolio. La suite facilita la colaboración sencilla al integrarse con Microsoft Teams, simplificando la accesibilidad, la asignación de recursos y la programación. También facilita la movilidad y es accesible en cualquier momento y lugar. Los paneles interactivos brindan a los usuarios análisis perspicaces, lo que permite una optimización más sencilla.",
+    classes: "border-0",
+  },
+  {
+    icon: "sql-database-maintenance.png",
+    title: "Mantenimiento de Bases de Datos SQL",
+    body: "Nuestro servicio de mantenimiento de bases de datos SQL garantiza la gestión completa de tu base de datos SQL, con monitoreo constante y resolución de problemas por parte de nuestro equipo dedicado de Administradores de Bases de Datos. Nuestros servicios proporcionan una base de datos optimizada y eficiente para complementar tus aplicaciones internas.",
+    classes: "border-0",
+  },
+  {
+    icon: "unified-communication-services.png",
+    title: "Servicios de Comunicación Unificada",
+    body: "Los servicios de comunicación unificada de New Era gestionan tu Microsoft Exchange, ya sea alojado de forma local o en la nube. Nuestros profesionales certificados te ayudan a migrar, mantener e implementar los buzones de correo de tu equipo y brindan servicios de soporte dentro de un SLA garantizado.",
+    classes: "border-0",
+  },
+];
