452e2f386c5e90d494907214128c4a614451af38
Luis U. <luis.urruela@neweratech.com>
2025-05-02 13:45:36 -0600

    Added images to use cases page and Updated contact page side modules with correct information

--- src/components/case-studies/cases.ts
+++ src/components/case-studies/cases.ts
@@ -1,31 +1,37 @@
+import image01 from 'src/assets/images/use-cases/01.jpg';
+import image02 from 'src/assets/images/use-cases/02.jpg';
+import image03 from 'src/assets/images/use-cases/03.jpg';
+import image04 from 'src/assets/images/use-cases/04.jpg';
+import image05 from 'src/assets/images/use-cases/05.jpg';
+
 export const cases = [
     {
         id: 1,
-        img: 'neweratechnology_logo.svg',
+        img: image01,
         title: 'Azure Data Lake',
         link: 'AzureDataLake.pdf'
     },
     {
         id: 2,
-        img: 'neweratechnology_logo.svg',
+        img: image02,
         title: 'Ciencia De Datos',
         link: 'CienciaDeDatos.pdf'
     },
     {
         id: 3,
-        img: 'neweratechnology_logo.svg',
+        img: image03,
         title: 'Gestion de Servicios TI',
         link: 'GestionServiciosTI.pdf'
     },
     {
         id: 4,
-        img: 'neweratechnology_logo.svg',
+        img: image04,
         title: 'Implementacion Power BI',
         link: 'ImplementacionPowerBi.pdf'
     },
     {
         id: 5,
-        img: 'neweratechnology_logo.svg',
+        img: image05,
         title: 'Nivel de Servicio',
         link: 'NivelDeServicio.pdf'
     }
